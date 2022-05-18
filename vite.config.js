4a2c7fa640f679c05bf20f629e039c6fcd3379e5
kapustyansky <kapustianskyov@gmail.com>
2022-05-18 23:33:18 +0300

    add. vite url

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-//import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -7,9 +7,9 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
 	base: '/upswot/',
 	plugins: [vue()],
-	// resolve: {
-	// 	alias: {
-	// 		'@': fileURLToPath(new URL('./src', import.meta.url)),
-	// 	},
-	// },
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 });
